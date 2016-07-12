9ca4c3d61e46d3e9f7e7f586c21b123b666d16f3
Dhru Upender <dhruti.upender@gmail.com>
2016-07-11 21:28:07 -0500

    ended class, unfinished code

--- Program.cs
+++ Program.cs
@@ -12,13 +12,20 @@ namespace Cheers
            //Variables Section
             String prompt = ">";
             String cheer_prefix = "Give me a";
+            String requires_an = "aeiolnfshrmx";
 
             Console.WriteLine("Whats your name?");
             Console.Write(prompt);
-            String user_name = Console.ReadLine();//Read from terminal save to a varialble
+            String user_name = Console.ReadLine().ToLower();//Read from terminal save to a varialble
 
             for(int i = 0; i < user_name.Length; i++){
+                if (requires_an.IndexOf(user_name[i])  > -1){
+                   //Then its in the requires_an array  
+                   Console.WriteLine(cheer_prefix + "n" + ".." + user_name[i]);
+                }else{
+                    //Does not require an "an"
                 Console.WriteLine(cheer_prefix + ".. " + user_name[i]);
+                }
             }
 
             //user_name.ToUpper() is turning the user's name to uppercase.
